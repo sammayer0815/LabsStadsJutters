303ed67a517ef400986c52916417c8699e0a566b
Your Name <you@example.com>
2024-06-04 02:44:01 +0200

    add and edit profile

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef  } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useHistory } from "react-router-dom";
 import { IonContent, IonHeader, IonBackButton, IonButtons, IonPage, IonRow, IonCol, IonGrid, IonButton, IonCard, IonInput, IonCardContent, IonText } from '@ionic/react';
 import Logo from '../assets/Logo.svg';
 import './Login.css';
+
+import { firestore } from '../config/controller';
+import { setDoc, doc } from 'firebase/firestore';
 
 const Register: React.FC = () => {
   const auth = getAuth();
@@ -12,32 +15,51 @@ const Register: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const [placeName, setPlaceName] = useState('');
   const [error, setError] = useState('');
+
+  // Refs to handle input fields 
+  // Couldn't use useState on these: an empty string would be send on the last filled field before submitting
+  const usernameRef = useRef<HTMLIonInputElement>(null);
+  const placeNameRef = useRef<HTMLIonInputElement>(null);
 
   const registerWithEmailAndPassword = async (event: any) => {
     event.preventDefault();
 
+    // Get username & placeName value from the input fields using refs
+    const username = usernameRef.current?.value as string;
+    const placeName = placeNameRef.current?.value as string;
+
+    // Confirm repeated password
     if (password !== confirmPassword) {
       setError("Passwords do not match");
       return;
     }
-    setAuthing(true);
+    setAuthing(true); // Set authing state to true to indicate the authentication process is ongoing
 
+    // Create new user using Firebase Authentication
     try {
       const response = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(response.user.uid);
+      const user = auth.currentUser; // Get the currently authenticated user
+
+      // If the user is authenticated, save their data in Firestore in an users document
+      if (user) {
+        await setDoc(doc(firestore, "users", user.uid), {
+          userId: user.uid,
+          username: username,
+          placeName: placeName,
+          profilePicture: user.photoURL,
+        });
+      }
+
+      // Redirect to the home page after successful registration
       history.replace("/home");
+
+      // Handle errors during registration
     } catch (error) {
       console.error("Error registering with email/password:", error);
       setError("Registration failed. Please try again.");
-      setAuthing(false);
+      setAuthing(false); // Reset authing state
     }
-  };
-
-  const goToLoginPage = () => {
-    history.push("/login");
   };
 
   return (
@@ -46,9 +68,11 @@ const Register: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
         <IonGrid fixed>
+          {/* Back btn */}
           <IonButtons>
             <IonBackButton defaultHref='/login'></IonBackButton>
           </IonButtons>
+          {/* Logo */}
           <IonRow class="ion-justify-content-center">
             <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="6">
               <div className='ion-text-center'>
@@ -56,6 +80,7 @@ const Register: React.FC = () => {
               </div>
             </IonCol>
           </IonRow>
+          {/* Description */}
           <IonRow class="ion-justify-content-center">
             <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="6" className='ion-text-center'>
               <hr/>
@@ -64,6 +89,7 @@ const Register: React.FC = () => {
               <p>Klaar om mee te doen?</p>
             </IonCol>
           </IonRow>
+          {/* Form card */}
           <IonRow class="ion-justify-content-center">
             <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="6">
               <IonCard>
@@ -71,11 +97,11 @@ const Register: React.FC = () => {
                   <form onSubmit={registerWithEmailAndPassword}>
                     <h2 className='ion-text-center title-form'>Aanmelden bij Stadsjutters</h2>
                     {error && <IonText color="danger">{error}</IonText>}
-                    <IonInput type='text' className="ion-margin-top register-input" mode="md" label="Gebruikersnaam" labelPlacement="floating" fill='outline' value={username} onIonChange={(e) => setUsername(e.detail.value!)} />
-                    <IonInput type='email' className="ion-margin-top register-input" mode="md" label="Email" labelPlacement="floating" fill='outline' value={email} onIonChange={(e) => setEmail(e.detail.value!)} />
-                    <IonInput type='password' className="ion-margin-top register-input" mode="md" label="Wachtwoord" labelPlacement="floating" fill='outline' value={password} onIonChange={(e) => setPassword(e.detail.value!)} />
-                    <IonInput type='password' className="ion-margin-top register-input" mode="md" label="Herhaal Wachtwoord" labelPlacement="floating" fill='outline' value={confirmPassword} onIonChange={(e) => setConfirmPassword(e.detail.value!)} />
-                    <IonInput type='text' className="ion-margin-top register-input" mode="md" label="Plaats" labelPlacement="floating" fill='outline' value={placeName} onIonChange={(e) => setPlaceName(e.detail.value!)} />
+                    <IonInput ref={usernameRef} type='text' className="ion-margin-top register-input" mode="md" label="Gebruikersnaam" name="username" labelPlacement="floating" fill='outline' />
+                    <IonInput type='email' className="ion-margin-top register-input" mode="md" label="Email" name="email" labelPlacement="floating" fill='outline'  onIonChange={(e) => setEmail(e.detail.value!)} />
+                    <IonInput type='password' className="ion-margin-top register-input" mode="md" label="Wachtwoord" name="password" labelPlacement="floating" fill='outline'  onIonChange={(e) => setPassword(e.detail.value!)} />
+                    <IonInput type='password' className="ion-margin-top register-input" mode="md" label="Herhaal Wachtwoord" name="confirmPassword" labelPlacement="floating" fill='outline' onIonChange={(e) => setConfirmPassword(e.detail.value!)} />
+                    <IonInput ref={placeNameRef} type='text' className="ion-margin-top register-input" mode="md" label="Plaats" name="placeName" labelPlacement="floating" fill='outline' />
                     <IonButton type='submit' mode='ios' className="ion-margin-top" expand='block' color='secondary' disabled={authing}>Register</IonButton>
                   </form>
                 </IonCardContent>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -5,10 +5,93 @@ import NavTabs from '../components/Nav';
 import './style.css';
 import TestImage4 from '../assets/Frans-eiken-set-rotated.jpg';
 
+
+import { useEffect, useState, useRef } from 'react';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { firestore } from '../config/controller';
+import { getAuth, updateEmail, reauthenticateWithCredential, EmailAuthProvider  } from "firebase/auth";
+
 const Settings: React.FC = () => {
+    const [userDetails, setUserDetails] = useState<any | null>(null);
+    const [password, setPassword] = useState('');
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+
+    const usernameRef = useRef<HTMLIonInputElement>(null);
+    const placeNameRef = useRef<HTMLIonInputElement>(null);
+
+    const auth = getAuth();
+    const user = auth.currentUser; 
+
+    // Fetch the current user's details from Firestore 
+    useEffect(() => {
+        const getUserDetails = async () => {
+            // Check if user is logged in
+            if (user) {
+                try { 
+                    // Creates a reference to the Firestore document for the current user using the user's UID.
+                    const userDoc = doc(firestore, "users", user.uid);
+                    // Fetch the document
+                    const docSnap = await getDoc(userDoc);
+                    
+                    // If the document exists, it sets the user details in setUserDetails()
+                    if (docSnap.exists()) {
+                        setUserDetails(docSnap.data());
+                    } else {
+                        console.log("No such document found!");
+                    }
+
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        };
+
+        getUserDetails();
+    }, [user]);
+
+    // const inputChange = (event: any) => {
+    //     const { name, value } = event.target;
+    //     setUserDetails((prevState: any) => ({
+    //         ...prevState,
+    //         [name]: value
+    //     }));
+    // };
+    
+    // const passwordChange = (event: any) => {
+    //     setPassword(event.target.value);
+    // };
+
+    const updateUserDetails = async (event: any) => {
+        event.preventDefault();
+
+        if (user) {
+            try {
+                // Update email in Firebase Authentication
+                if (newEmail !== user.email && newEmail !== '') {
+                    await updateEmail(user, newEmail);
+                }
+
+                // Creates a reference to the Firestore document for the current user using the user's UID.
+                const userDoc = doc(firestore, "users", user.uid)
+                await updateDoc(userDoc, {
+                    username: usernameRef.current?.value,
+                    placeName: placeNameRef.current?.value,
+                });
+                alert('User details updated successfully!');
+            } catch (error) {
+                console.error("Error updating details: ", error);
+                alert('Failed to update user details.');
+            }
+        }
+    };
+
+
+
 
     return (
         <IonPage>
+            {/* Back btn to profile */}
             <IonHeader>
                 <IonToolbar color={'secondary'} className='custom-toolbar' mode='ios'>
                     <IonButtons>
@@ -19,6 +102,7 @@ const Settings: React.FC = () => {
             </IonHeader>
             <IonContent className="ion-padding">
                 <IonGrid fixed>
+                    {/* Display profile and change profile picture */}
                     <IonRow class="ion-justify-content-center">
                         <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
                             <div className='flex-profile-picture-text'>
@@ -48,18 +132,24 @@ const Settings: React.FC = () => {
                             <hr className='ion-margin-top'/>
                         </IonCol>
                     </IonRow>
+                    {/* Form of user details */}
                     <IonRow class="ion-justify-content-center">
                         <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
-                            <form action="" >
-                                <h2 className='ion-text-center title-form'>Aanmelden bij Stadsjutters</h2>
-                                <IonInput type='text' className="ion-margin-top register-input" mode="md" label="Gebruikersnaam" labelPlacement="floating" fill='outline' value={'Henk123'}  ></IonInput>
-                                <IonInput type='email' className="ion-margin-top register-input" mode="md" label="Email" labelPlacement="floating" fill='outline' placeholder='test@gmail.com' value={'test@gmail.com'} ></IonInput>
-                                <IonInput type='password' className="ion-margin-top register-input" mode="md" label="Nieuw Wachtwoord" labelPlacement="floating" fill='outline'></IonInput>
-                                <IonInput type='text' className="ion-margin-top register-input" mode="md" label="Plaats" labelPlacement="floating" fill='outline' value={'Amsterdam'} ></IonInput>
-                                <IonButton type='submit' mode="ios" className="ion-margin-top" expand='block' color={'secondary'}>Gegevens updaten</IonButton>
-                            </form>
+                            {/*  If userDetails exists, show the form */}
+                            {userDetails && (
+                                <form onSubmit={updateUserDetails}>
+                                    <h2 className='ion-text-center title-form'>Aanmelden bij Stadsjutters</h2>
+                                    <IonInput type='text' className="ion-margin-top register-input" mode="md" label="Gebruikersnaam" labelPlacement="floating" fill='outline' name="username" value={userDetails.username} ref={usernameRef}></IonInput>
+                                    <IonInput type='email' className="ion-margin-top register-input" mode="md" label="Email" labelPlacement="floating" fill='outline' name="email" value={user?.email || ''} onIonChange={(e) => setNewEmail(e.detail.value!)}></IonInput>
+                                    <IonInput type='password' className="ion-margin-top register-input" mode="md" label="Nieuw Wachtwoord" labelPlacement="floating" fill='outline' name="password"></IonInput>
+                                    <IonInput type='text' className="ion-margin-top register-input" mode="md" label="Plaats" labelPlacement="floating" fill='outline' name="placeName"  value={userDetails.placeName} ref={placeNameRef} ></IonInput>
+                                    <IonButton type='submit' mode="ios" className="ion-margin-top" expand='block' color={'secondary'}>Gegevens updaten</IonButton>
+                                </form>
+                            )}
+                            
                         </IonCol>
-                    </IonRow>
+                    </IonRow> 
+                    {/* Delete profile btn */}
                     <IonRow class="ion-justify-content-center ion-margin-top">
                         <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
                             <hr className='ion-margin-bottom'/>
